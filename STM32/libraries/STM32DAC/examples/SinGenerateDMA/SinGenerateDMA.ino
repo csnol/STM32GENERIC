363ddb084a02d7912f1e3daecae94da351bf8ff5
huaweiwx <huaweiwx@sina.com>
2019-12-20 23:51:38 +0800

    dma use uint32_t array only

--- STM32/libraries/STM32DAC/examples/SinGenerateDMA/SinGenerateDMA.ino
+++ STM32/libraries/STM32DAC/examples/SinGenerateDMA/SinGenerateDMA.ino
@@ -10,12 +10,12 @@
 
 #define NUMS    72
 
-static uint16_t wavedatas [NUMS];
+static uint32_t wavedatas[NUMS]; /*dma use uint32_t array only*/
 
-static uint16_t myfunc(int i) {
+static uint32_t myfunc(int i) {
   int mul = 10000;
   int res = sin(2 * PI / NUMS * i) * mul;
-  return (uint16_t) map(res, -mul, mul,  0, 0xfff);
+  return (uint32_t) map(res, -mul, mul,  0, 0xfff);
 }
 
 // the setup routine runs once when you press reset:
@@ -23,9 +23,9 @@ void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   delay(1000);
-  
+
   Serial.println("Sin generate with DMA demo...");
-  
+
   for (int i = 0; i < NUMS; i++) wavedatas[i] = myfunc(i);
 
   PA4_DACOUT.Init();
@@ -33,6 +33,5 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  PA4_DACOUT.startDMA(wavedatas,NUMS);	
+  PA4_DACOUT.startDMA(wavedatas, NUMS);
 }
-
